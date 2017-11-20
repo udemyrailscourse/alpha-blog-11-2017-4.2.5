987a6278a263d4ad428a0d800478f6f8c01431ff
Test team 1 <tddror5railsudemytest1@gmail.com>
2017-11-20 19:47:16 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
+  
+  resources :categories, except: [:destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
